9474d123d7c1d94881f95961aff57ec8d9b65fe1
nqkhanh2002 <20521452@gm.uit.edu.vn>
2023-06-06 13:02:58 +0700

    upload

---
+++ app/config.py
@@ -0,0 +1,19 @@
+datafields = "D:\DESKTOP\Rossman\data\datafields.csv"
+
+nav_image = "D:\DESKTOP\Rossman\data\navbar.png"
+home_image = "D:\DESKTOP\Rossman\data\home.png"
+
+quarter_1 = "D:\DESKTOP\Rossman\data\q1.csv"
+quarter_2 = "D:\DESKTOP\Rossman\data\q2.csv"
+quarter_3 = "D:\DESKTOP\Rossman\data\q3.csv"
+quarter_4 = "D:\DESKTOP\Rossman\data\q4.csv"
+
+quarter_1_datasales = "D:\DESKTOP\Rossman\data\q1_datesales.csv"
+quarter_2_datasales = "D:\DESKTOP\Rossman\data\q2_datesales.csv"
+quarter_3_datasales = "D:\DESKTOP\Rossman\data\q3_datesales.csv"
+quarter_4_datasales = "D:\DESKTOP\Rossman\data\q4_datesales.csv"
+
+features = ["DayOfWeek","DayOfWeekName","MonthName","Customers","Promo","StateHoliday","SchoolHoliday","StoreType","Assortment","CompetitionDistance","CompetitionOpenSinceMonth","Promo2SinceWeek","PromoInterval"]
+salesDependingFeatures = [6,2,6,437,0,0,0,0,0,4520.0,2.0,14.0,1]
+
+model_load = "D:\DESKTOP\Rossman\model\sales_prediction.sav"
