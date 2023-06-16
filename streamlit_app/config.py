ffb6ae1e6e7eb231812708754652f2dc5d116cbe
nqkhanh2002 <20521452@gm.uit.edu.vn>
2023-06-17 03:25:51 +0700

    update

--- streamlit_app/config.py
+++ streamlit_app/config.py
@@ -1,20 +1,20 @@
-datafields = "data/datafields.csv"
+datafields = "../data/datafields.csv"
 
-nav_image = "data/Logo_UIT.jpg"
-home_image = "data/home.jpg"
+nav_image = "../data/Logo_UIT.jpg"
+home_image = "../data/home.jpg"
 
-quarter_1 = "data/q1.csv"
-quarter_2 = "data/q2.csv"
-quarter_3 = "data/q3.csv"
-quarter_4 = "data/q4.csv"
+quarter_1 = "../data/q1.csv"
+quarter_2 = "../data/q2.csv"
+quarter_3 = "../data/q3.csv"
+quarter_4 = "../data/q4.csv"
 
-quarter_1_datasales = "data/q1_datesales.csv"
-quarter_2_datasales = "data/q2_datesales.csv"
-quarter_3_datasales = "data/q3_datesales.csv"
-quarter_4_datasales = "data/q4_datesales.csv"
+quarter_1_datasales = "../data/q1_datesales.csv"
+quarter_2_datasales = "../data/q2_datesales.csv"
+quarter_3_datasales = "../data/q3_datesales.csv"
+quarter_4_datasales = "../data/q4_datesales.csv"
 
 features = ["DayOfWeek","Customers", "Promo", "StateHoliday", "SchoolHoliday", "StoreType", "Assortment", "CompetitionDistance", 
             "CompetitionOpenSinceMonth","Promo2SinceWeek", "PromoInterval", "MonthName"]
-salesDependingFeatures = [2,0,0,1,1,3, 2, 5350.00,4.00	,22.00,	2	,6]
-
-model_load = "model/sales_prediction.sav"
+# salesDependingFeatures = [[3,319,0,0,0,3,2,5350.00,4.00,22.00,2,5]]
+salesDependingFeatures = [3,319,0,0,0,3,2,5350.00,4.00,22.00,2,6]
+model_load = "../model/sales_prediction.sav"
--- streamlit_app/model.py
+++ streamlit_app/model.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 
 from config import *
-
+from keras.models import load_model
 #feature update
 def feature_update(li,features):
     final_features = []
@@ -40,9 +40,27 @@ def app():
     d ={"Feature ":features, "Value for Prediction": salesDependingFeatures}
     st.subheader("Default Values")
     st.write(pd.DataFrame(d))
+
+    
     loaded_model = pickle.load(open(model_load, 'rb'))
     prediction_data = pd.DataFrame([salesDependingFeatures],
                                columns=features)
-    model_ = loaded_model.predict(prediction_data)
-    st.subheader("The Predicted Value")
-    st.write(model_)
+    
+    model_ = loaded_model.predict(prediction_data).flatten()
+    loaded_model_DL = load_model(r"..\model\model.h5").predict(prediction_data).flatten()
+    # st.subheader("The Predicted Value - XGBRegressor")
+    # st.write(model_)
+    # st.subheader("The Predicted Value - Deep Learning Model")
+    # st.write(loaded_model_DL)
+    # Create a dictionary to store the predictions
+
+    d = {
+        "XGBRegressor Predicted Value": model_,
+        "Deep Learning Predicted Value": loaded_model_DL
+    }
+
+    # Create a DataFrame from the dictionary
+    predictions_df = pd.DataFrame(d)
+
+    # Display the predictions in a table
+    st.write(predictions_df)
