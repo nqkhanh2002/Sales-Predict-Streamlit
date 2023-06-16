7d6d358a7651f6a21d4e289818afceef3034db0f
nqkhanh2002 <20521452@gm.uit.edu.vn>
2023-06-17 03:35:38 +0700

    update

--- streamlit_app/config.py
+++ streamlit_app/config.py
@@ -1,20 +1,19 @@
-datafields = "../data/datafields.csv"
+datafields = r"../data/datafields.csv"
 
-nav_image = "../data/Logo_UIT.jpg"
-home_image = "../data/home.jpg"
+nav_image = r"../data/Logo_UIT.jpg"
+home_image = r"../data/home.jpg"
 
-quarter_1 = "../data/q1.csv"
-quarter_2 = "../data/q2.csv"
-quarter_3 = "../data/q3.csv"
-quarter_4 = "../data/q4.csv"
+quarter_1 = r"../data/q1.csv"
+quarter_2 = r"../data/q2.csv"
+quarter_3 = r"../data/q3.csv"
+quarter_4 = r"../data/q4.csv"
 
-quarter_1_datasales = "../data/q1_datesales.csv"
-quarter_2_datasales = "../data/q2_datesales.csv"
-quarter_3_datasales = "../data/q3_datesales.csv"
-quarter_4_datasales = "../data/q4_datesales.csv"
+quarter_1_datasales = r"../data/q1_datesales.csv"
+quarter_2_datasales = r"../data/q2_datesales.csv"
+quarter_3_datasales = r"../data/q3_datesales.csv"
+quarter_4_datasales = r"../data/q4_datesales.csv"
 
 features = ["DayOfWeek","Customers", "Promo", "StateHoliday", "SchoolHoliday", "StoreType", "Assortment", "CompetitionDistance", 
             "CompetitionOpenSinceMonth","Promo2SinceWeek", "PromoInterval", "MonthName"]
-# salesDependingFeatures = [[3,319,0,0,0,3,2,5350.00,4.00,22.00,2,5]]
 salesDependingFeatures = [3,319,0,0,0,3,2,5350.00,4.00,22.00,2,6]
-model_load = "../model/sales_prediction.sav"
+model_load = r"../model/sales_prediction.sav"
