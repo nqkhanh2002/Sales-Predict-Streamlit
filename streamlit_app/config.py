1d8e8d895a0aa5dd9571c83af5a811922ba948b7
nqkhanh2002 <20521452@gm.uit.edu.vn>
2023-06-17 04:03:29 +0700

    cc

--- streamlit_app/config.py
+++ streamlit_app/config.py
@@ -1,17 +1,17 @@
 datafields = "../data/datafields.csv"
 
-nav_image = "../data/Logo_UIT.jpg"
-home_image = "../data/home.jpg"
+nav_image = "data/Logo_UIT.jpg"
+home_image = "data/home.jpg"
 
-quarter_1 = "../data/q1.csv"
-quarter_2 = "../data/q2.csv"
-quarter_3 = "../data/q3.csv"
-quarter_4 = "../data/q4.csv"
+quarter_1 = "data/q1.csv"
+quarter_2 = "data/q2.csv"
+quarter_3 = "data/q3.csv"
+quarter_4 = "data/q4.csv"
 
-quarter_1_datasales = r"../data/q1_datesales.csv"
-quarter_2_datasales = r"../data/q2_datesales.csv"
-quarter_3_datasales = r"../data/q3_datesales.csv"
-quarter_4_datasales = r"../data/q4_datesales.csv"
+quarter_1_datasales = "data/q1_datesales.csv"
+quarter_2_datasales = "data/q2_datesales.csv"
+quarter_3_datasales = "data/q3_datesales.csv"
+quarter_4_datasales = "data/q4_datesales.csv"
 
 features = ["DayOfWeek","Customers", "Promo", "StateHoliday", "SchoolHoliday", "StoreType", "Assortment", "CompetitionDistance", 
             "CompetitionOpenSinceMonth","Promo2SinceWeek", "PromoInterval", "MonthName"]
